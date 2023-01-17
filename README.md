d727776719976a334f887874b3fca8ade4e7e744
Ilia Sharin <isharin@gmail.com>
2023-01-16 19:55:51 -0500

    Initial code drop

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # drogon-boilerplate
 C++ Boilerplate starter for Drogon framework-based websevice
+
+
+    git submodule update --init --recursive
+    mkdir build
+    cd build
+    cmake ..
+    make
---
+++ src/hello_world_api.cpp
@@ -0,0 +1,19 @@
+#include <drogon/HttpController.h>
+
+namespace api
+{
+    namespace v1
+    {
+        class HelloWorld : public drogon::HttpController<HelloWorld>
+        {
+        public:
+            HelloWorld() = default;
+
+            METHOD_LIST_BEGIN
+            METHOD_ADD(HelloWorld::get, "", drogon::Get); //path is /api/v1/helloworld
+            METHOD_LIST_END
+
+            void get(const drogon::HttpRequestPtr &req, std::function<void(const drogon::HttpResponsePtr &)> &&callback) const;
+        };
+    } // namespace v1
+} // namespace api
